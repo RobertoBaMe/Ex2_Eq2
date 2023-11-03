ad19f61becd2df7a98ec482717dd8710f1880e8f
RobertoBaMe <roberto.barraza@uniat.edu.mx>
2023-11-02 21:51:31 -0600

    Creación de menú principal
    Se le coloca tipografía al menú principal y se le agrega Lean Tween para que quede mamalón.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject title;
+    [SerializeField] private GameObject monoRojo;
+    [SerializeField] private GameObject monoAmarillo;
+    [SerializeField] private GameObject monoAzul;
+
+    private void Start()
+    {
+        // LeanTween.scale(title, new Vector3(1, 1, 1),1).setEase(LeanTweenType.easeInExpo).setDelay(1);
+        LeanTween.scale(title, new Vector3(1, 1, 1), 1f).setLoopType(LeanTweenType.pingPong);
+        LeanTween.moveX(monoAmarillo, 850.0f, 4.0f);
+        LeanTween.moveY(monoAzul, 350.0f, 1.0f).setDelay(6.5f);
+        LeanTween.moveX(monoAzul, 50.0f, 1.0f).setDelay(6.5f);
+        LeanTween.moveY(monoRojo, 50.0f, 6f).setEase(LeanTweenType.easeInBounce).setDelay(9.0f);
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
     }
 }
