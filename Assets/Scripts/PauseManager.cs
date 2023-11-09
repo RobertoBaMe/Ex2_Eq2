98c4d294b77f7c0da0ff64f4f5995dcb00dc41df
RobertoBaMe <roberto.barraza@uniat.edu.mx>
2023-11-08 23:20:51 -0600

    WinPanel
    Se agrega panel de victoria y su contenido

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -6,18 +6,23 @@ using UnityEngine.SceneManagement;
 public class PauseManager : MonoBehaviour
 {
     private bool gameIsPaused;
-    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject pauseMenu, winMenu, colorWheel;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)) 
         {
             gameIsPaused = !gameIsPaused;
             PauseGame();
         }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            WinFuction();
+        }
     }
 
-    void PauseGame()
+    void PauseGame() //Pausar juego
     {
         if (gameIsPaused)
         {
@@ -31,8 +36,15 @@ public class PauseManager : MonoBehaviour
         }
     }
 
-    public void ReturnMenu()
+    public void ReturnMenu() //Regresar a menú principal
     {
         SceneManager.LoadScene(0);
+    }
+
+    private void WinFuction() //Función de victoria
+    {
+        //Time.timeScale = 0;
+        winMenu.SetActive(true);
+        LeanTween.rotateAround(colorWheel, Vector3.forward, -360, 10f).setLoopClamp();
     }
 }
