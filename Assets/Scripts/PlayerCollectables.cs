43668df372ebb86479318a4b273a7661fbee06e1
Oscar Aceves <oaceves@classvaluation.com>
2023-11-09 21:16:03 -0600

    Player Sword and Diamond prefabs

--- Assets/Scripts/PlayerCollectables.cs
+++ Assets/Scripts/PlayerCollectables.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class PlayerCollectables : MonoBehaviour
 {
+    private bool haveKey = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision != null && collision.gameObject.CompareTag("Collectable")) { 
-            //TODO: Aumentar vida o contador en la UI
-            collision.gameObject.SetActive(false);
+        if (collision != null)
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Collectable":
+                    //TODO: Aumentar vida o contador en la UI
+                    collision.gameObject.SetActive(false);
+                    break;
+                case "Key":
+                    haveKey = true;
+                    collision.gameObject.SetActive(false);
+                    break;
+                case "Chest":
+                    if (haveKey)
+                    {
+                        collision.gameObject.SetActive(false);
+                        UnityEngine.Debug.Log("Ganaste");
+                    }
+                    else {
+                        UnityEngine.Debug.Log("Necesitas la Llave");
+                    }
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 }
